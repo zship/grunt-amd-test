3d0c445c2df376fe9164b74a7bbd055661385e6a
Zach Shipley <zach@zachshipley.com>
2013-02-09 16:22:09 -0600

    Added support for "mainConfigFile" requirejs property

--- tasks/test.js
+++ tasks/test.js
@@ -2,55 +2,17 @@ module.exports = function(grunt) {
 	'use strict';
 
 	var path = require('path');
-	//var fs = require('fs');
 	var _ = grunt.utils._;
 	var jade = require('jade');
-	//var handlebars = require('handlebars');
 	var util = require('./util.js');
 
 	var testDir = path.resolve(process.cwd() + '/test');
-	var specDir = path.resolve(testDir + '/unit');
-	//var specTplPath = path.resolve(__dirname + '/tpl/spec.hbs');
 	var testRunner = path.resolve(__dirname + '/tpl/runner.jade');
-
-
-	//var specTpl;
-
-/*
- *    var _generateFailing = function(src, config) {
- *        var moduleList = [];
- *
- *        src.forEach(function(file) {
- *            var module = util.fileToModuleName(file, config);
- *            moduleList.push(module);
- *
- *            file = specDir + '/' + module + '.js';
- *            if (fs.existsSync(file)) {
- *                return true;
- *            }
- *
- *            var name = module.split('/').pop();
- *
- *            specTpl = specTpl || handlebars.compile(grunt.file.read(specTplPath, 'utf-8').toString());
- *            var data = specTpl({module: module, name: name});
- *            grunt.file.write(file, data, 'utf-8');
- *        });
- *
- *        return moduleList;
- *    };
- */
 
 
 	var _generateRunner = function(mode, config, modules) {
 		config = _.clone(config);
 		config.baseUrl = path.relative(testDir, config.baseUrl);
-
-		/*
-		 *modules = modules.map(function(file) {
-		 *    console.log(file);
-		 *    return util.fileToModuleName(file, config);
-		 *});
-		 */
 
 		modules = modules.map(function(mod) {
 			return path.relative(testDir, path.resolve(process.cwd() + '/' + mod));
@@ -95,28 +57,31 @@ module.exports = function(grunt) {
 
 	grunt.registerTask('test', 'Generates QUnit/jasmine html and optionally runs QUnit/jasmine', function() {
 		var config = grunt.config.get(this.name);
-		var rjsconfig = grunt.config.get('requirejs');
+		var done = this.async();
 
-		config.include = util.expand(config.include);
-		config.exclude = util.expand(config.exclude);
+		util.loadConfig(grunt.config.get('requirejs')).then(function(rjsconfig) {
+			config.include = util.expand(config.include);
+			config.exclude = util.expand(config.exclude);
 
-		var modules = _.difference(config.include, config.exclude);
+			var modules = _.difference(config.include, config.exclude);
 
-		//var modules = _generateFailing(config.include, rjsconfig);
-		var file = _generateRunner(config.mode, rjsconfig, modules);
+			var file = _generateRunner(config.mode, rjsconfig, modules);
 
-		if (!config.run) {
-			return;
-		}
+			if (!config.run) {
+				return;
+			}
 
-		var serverPath = 'http://localhost:';
-		serverPath += grunt.config.get('server').port + '/';
-		serverPath += path.relative(grunt.config.get('server').base, path.resolve(file));
+			var serverPath = 'http://localhost:';
+			serverPath += grunt.config.get('server').port + '/';
+			serverPath += path.relative(grunt.config.get('server').base, path.resolve(file));
 
-		grunt.config.set('qunit', {
-			test: serverPath
+			grunt.config.set('qunit', {
+				test: serverPath
+			});
+
+			grunt.task.run(['server', 'qunit:test']);
+			done();
 		});
 
-		grunt.task.run(['server', 'qunit:test']);
 	});
 };
--- tasks/util.js
+++ tasks/util.js
@@ -3,7 +3,9 @@
 var fs = require('fs');
 var path = require('path');
 var grunt = require('grunt/lib/grunt.js');
+var mixin = require('mout/object/deepMixIn');
 var _ = grunt.utils._;
+var Deferred = require('deferreds.js/Deferred');
 
 
 var util = {
@@ -25,6 +27,32 @@ var util = {
 		});
 
 		return _.uniq(files);
+	},
+
+
+	loadConfig: function(config) {
+		var deferred = new Deferred();
+
+		if (config.mainConfigFile) {
+			if (!fs.existsSync(config.mainConfigFile)) {
+				throw new Error('requirejs config: mainConfigFile property: file cannot be found');
+			}
+
+			var requirejs = require('./lib/r.js');
+			requirejs.config({
+				baseUrl: __dirname,
+				nodeRequire: require
+			});
+
+			requirejs(['./lib/parse'], function(parse) {
+				var mainConfig = parse.findConfig(grunt.file.read(config.mainConfigFile)).config;
+				deferred.resolve(mixin({}, mainConfig, config));
+			});
+			return deferred.promise();
+		}
+
+		deferred.resolve(config);
+		return deferred.promise();
 	},
 
 
@@ -51,35 +79,99 @@ var util = {
 		var packages = rjsconfig.packages || [];
 		var transforms = _.map(paths, function(val, key) {
 			return {
-				from: key,
-				to: val
+				from: val,
+				to: key
 			};
 		}).concat(packages.map(function(pkg) {
-		return {
-			from: pkg.location,
-			to: pkg.name
-		};
+			return {
+				from: pkg.location,
+				to: pkg.name
+			};
 		}));
 
 		//console.log(relativePath);
 
 		_.chain(transforms)
-		.sortBy(function(obj) {
-			//transform in order from most complex to simplest
-			return -1 * obj.from.length;
-		})
-		.every(function(obj) {
-			if (relativePath.search(obj.from) !== -1) {
-				relativePath = relativePath.replace(obj.from, obj.to);
-				return false;
-			}
-			return true;
-		});
+			.sortBy(function(obj) {
+				//transform in order from most complex to simplest
+				return -1 * obj.from.length;
+			})
+			.every(function(obj) {
+				if (relativePath.search(obj.from) !== -1) {
+					relativePath = relativePath.replace(obj.from, obj.to);
+					return false;
+				}
+				return true;
+			});
 
 
 		//console.log(relativePath);
 
 		return relativePath.replace('.js', '');
+	},
+
+
+	moduleToFileName: function(declaredName, directory, rjsconfig) {
+		declaredName = declaredName.replace(/\.js/, '');
+
+		var candidate;
+
+		//relative paths
+		if (declaredName.search(/^\./) !== -1) {
+			candidate = path.normalize(directory + '/' + declaredName + '.js');
+
+			if (fs.existsSync(candidate)) {
+				return candidate;
+			}
+			else {
+				return undefined;
+			}
+		}
+
+		//non-transformed paths
+		candidate = path.resolve(process.cwd() + '/' + rjsconfig.baseUrl + '/' + declaredName + '.js');
+
+		if (fs.existsSync(candidate)) {
+			return candidate;
+		}
+		
+		//transformed paths (have 'paths' or 'packages' entries in rjsconfig)
+		var paths = rjsconfig.paths || [];
+		var packages = rjsconfig.packages || [];
+		var transforms = _.map(paths, function(val, key) {
+			return {
+				from: key,
+				to: val
+			};
+		}).concat(packages.map(function(pkg) {
+			return {
+				from: pkg.name,
+				to: pkg.location
+			};
+		}));
+
+		var result;
+
+		_.chain(transforms)
+			.sortBy(function(obj) {
+				//transform in order from most complex to simplest
+				return -1 * obj.from.length;
+			})
+			.every(function(obj) {
+				var candidate = declaredName;
+				if (candidate.search(obj.from) !== -1) {
+					candidate = candidate.replace(obj.from, obj.to);
+					candidate = path.resolve(process.cwd() + '/' + rjsconfig.baseUrl + '/' + candidate + '.js');
+					if (fs.existsSync(candidate)) {
+						result = candidate;
+						return false;
+					}
+				}
+				return true;
+			});
+
+		return result;
+
 	}
 
 };
